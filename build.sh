1ff23e4340ff72c62e55ee8931f692c38d28448c
Marius PvW <marius@taniustech.com>
2026-03-30 09:14:39 -0400

    static release

--- build.sh
+++ build.sh
@@ -5,16 +5,21 @@
 
 set -e  # Exit on error
 
-# Check if --debug flag is provided
+# --debug => Debug; --release-static => Release, static SDL2 required, separate build dir
 if [[ "$1" == "--debug" ]]; then
     BUILD_TYPE="Debug"
+    BUILD_SUBDIR="Debug"
+elif [[ "$1" == "--release-static" ]]; then
+    BUILD_TYPE="Release"
+    BUILD_SUBDIR="Release-static"
 else
     BUILD_TYPE="Release"
+    BUILD_SUBDIR="Release"
 fi
 
 # Get the project root directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BUILD_DIR="${SCRIPT_DIR}/build/${BUILD_TYPE}"
+BUILD_DIR="${SCRIPT_DIR}/build/${BUILD_SUBDIR}"
 
 echo "Building Mandel in ${BUILD_TYPE} mode..."
 
@@ -22,6 +27,9 @@ echo "Building Mandel in ${BUILD_TYPE} mode..."
 CMAKE_OPTS=(-B "${BUILD_DIR}" -S "${SCRIPT_DIR}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}")
 if [[ "${BUILD_TYPE}" == "Release" ]]; then
     CMAKE_OPTS+=(-DSTATIC_LINKING=ON)
+fi
+if [[ "$1" == "--release-static" ]]; then
+    CMAKE_OPTS+=(-DSTATIC_LINKING_STRICT=ON)
 fi
 echo "Configuring CMake..."
 cmake "${CMAKE_OPTS[@]}"
